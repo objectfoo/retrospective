32ce2f446428ea7cbdffdf297976a687b7710dba
Satake <objectfoo@users.noreply.github.com>
2023-08-26 20:40:58 -0700

    get typed eslint working

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,49 @@
 module.exports = {
 	root: true,
-	env: { browser: true, es2020: true },
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:react-hooks/recommended',
-		"eslint-config-prettier",
-	],
-	ignorePatterns: ['dist', '.eslintrc.cjs'],
-	parser: '@typescript-eslint/parser',
-	plugins: ['react-refresh'],
+	env: { node: true },
+	extends: [ "eslint:recommended" ],
+	ignorePatterns: ["dist","vite.config.ts"],
 	rules: {
-		"quotes": "off",
-		"@typescript-eslint/quotes": ["error", "double"],
-		"semi": "off",
-		"@typescript-eslint/semi": "error",
+		"quotes": ["error", "double"],
+		"semi": "error",
 		"indent": ["error", "tab"],
-		"no-unused-vars": "off",
-		"@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
-		"@typescript-eslint/explicit-function-return-type": "error",
-		'react-refresh/only-export-components': ['warn', { allowConstantExport: true },],
+		"no-unused-vars": "error",
 	},
-}
+	overrides: [
+		{
+			files: ["./src/**/*"],
+			"settings": {
+				"react": {
+					"version": "detect"
+				}
+			},
+			env: { browser: true, es2020: true },
+			parser: "@typescript-eslint/parser",
+			parserOptions: {
+				jsx: true,
+				project: true,
+				tsconfigRootDir: __dirname,
+			},
+			extends: [
+				"eslint:recommended",
+				"plugin:@typescript-eslint/strict-type-checked",
+				"plugin:@typescript-eslint/stylistic-type-checked",
+				"plugin:react-hooks/recommended",
+				"plugin:react/recommended",
+				"plugin:react/jsx-runtime",
+				"eslint-config-prettier",
+			],
+			plugins: ["react-refresh"],
+			rules: {
+				"quotes": "off",
+				"@typescript-eslint/quotes": ["error", "double"],
+				"semi": "off",
+				"@typescript-eslint/semi": "error",
+				"no-unused-vars": "off",
+				"@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+				"@typescript-eslint/explicit-function-return-type": "error",
+				"react-refresh/only-export-components": ["warn", { allowConstantExport: true },],
+			},
+		}
+	]
+};
